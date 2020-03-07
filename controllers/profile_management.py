009b54dbdbdea5a1787d988b5b78a66ccb026e29
yashar <yasharghaemi@gmail.com>
2020-03-07 12:42:01 -0500

    start welcome message

--- controllers/profile_management.py
+++ controllers/profile_management.py
@@ -13,3 +13,14 @@ def insert_user(user_id, name):
 def update_user(user_id, field, value):
     user_profile.update_by_uid(user_id, field, value)
 
+
+def get_start_message(effective_user):
+    user = search_user(effective_user["id"])
+    if user is None:
+        insert_user(effective_user["id"], effective_user["first_name"])
+        return "Hello %s and Welcome to ShopBot \n We advice to complete your user profile to search products easily." % str(effective_user["first_name"])
+    else:
+        return "Hello and welcome back %s" % user["first_name"]
+
+
+
